0cd2f2c1170a5f920db1dc2c07fb142c0e9463c9
earthflex <earthflexible@gmail.com>
2023-03-28 17:05:05 +0700

    url

--- component/meta.js
+++ component/meta.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 
-const DOMAIN = "https://2275-49-49-233-191.ap.ngrok.io"
-// const DOMAIN = "https://next-auth-earthflex.vercel.app"
+// const DOMAIN = "https://2275-49-49-233-191.ap.ngrok.io"
+const DOMAIN = "https://next-auth-earthflex.vercel.app"
 
 export default function Meta({
   title = "Next Auth & Chakra UI",
